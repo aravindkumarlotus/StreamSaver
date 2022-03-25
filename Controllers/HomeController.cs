bc15da31acbcb5e0e45cd3fb61120564559b33f5
Aravind K <aravind@empressmam.com>
2022-03-25 22:11:01 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreamSaver.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult mitm()
+        {
+            return new FilePathResult("~/Views/Home/mitm.html", "text/html");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StreamSaver.MvcApplication" Language="C#" %>
